acc774f2d07b952299778488b2e7b4c1418a68e2
kcrikana <beom0109@naver.com>
2021-05-05 04:18:41 +0900

    Initial commit

---
+++ src/main/java/kr/mjc/minbeom/web/dao/User.java
@@ -0,0 +1,18 @@
+package kr.mjc.minbeom.web.dao;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private int userId;
+    private String email;
+    private String name;
+    private String password;
+
+    @Override
+    public String toString() {
+        return "User{" + "userId=" + userId + ", email='" + email + '\'' +
+                ", name='" + name + '\'' + '}';
+    }
+}
+
